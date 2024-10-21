9938ee3814f7500c0afe66b728d997a232a9b69c
xcalie <2833186964@qq.com>
2024-10-22 01:19:14 +0800

    10.22

--- 312_delegate/Program.cs
+++ 312_delegate/Program.cs
@@ -3,12 +3,48 @@
     delegate void MyAction();
     delegate int MyFunc(int a);
     // 规则与重载与函数一样
+
+
+    class Test
+    {
+        public MyAction action;
+        public MyFunc func;
+        
+        
+        public void Testfun( MyFunc myfun,  MyAction myAction)
+        {
+            int i = 1;
+            i *= 2;
+            i += 2;
+            
+            this.func = myfun;
+            this.action = myAction;
+        }
+    }
+
+
     internal class Program
     {
-        MyAction func;
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            
+            
+            test.Testfun(test.func, test.action);
+        }
+         static void Fun()
+        {
+            Console.WriteLine("123123");
+        }
         
-
         
+        static void Fun2(int a)
+        {
+            Console.WriteLine(a);
+        }
         
     }
+    
+   
+
 }
