5c544b6e066f8e21dacb7eec721b35d313fc0993
xcalie <2833186964@qq.com>
2024-10-23 02:02:36 +0800

    10.23

--- 312_delegate/Program.cs
+++ 312_delegate/Program.cs
@@ -2,6 +2,7 @@
 {
     delegate void MyAction();
     delegate int MyFunc(int a);
+    delegate T MyFunc2<T>(T a);
     // 规则与重载与函数一样
 
 
@@ -30,7 +31,21 @@
             Test test = new Test();
             
             
-            test.Testfun(test.func, test.action);
+            test.Testfun(Fun2, Fun);
+
+
+            MyAction f = null;
+            f += Fun;
+            f();
+            // 多播委托可以同时调用多次
+            f += Fun;
+
+            f();
+            
+            string str = "Hello";
+            MyFunc2<string> myFunc2 = null;
+            myFunc2 = Fun3;
+            Console.WriteLine(myFunc2(str));
         }
          static void Fun()
         {
@@ -38,14 +53,20 @@
         }
         
         
-        static void Fun2(int a)
+        static int Fun2(int a)
         {
             Console.WriteLine(a);
-            
+            return 1;
+        }
+
+        static T Fun3<T>(T a)
+        {
+            return a;
         }
         
     }
     
    
-
+    // Action 无返回值
+    // Func 有返回值
 }
